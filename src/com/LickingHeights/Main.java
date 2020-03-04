5795b8578317878e08a9a47881eecab28b390bb6
makenna <makenna.gordon.2023@student.lhschools.org>
2020-03-04 13:25:37 -0500

    intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,36 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("+-----------------------------------------+");
+	System.out.println("|                GAS STATION              |");
+	System.out.println("|                   SUNOCO                |");
+	System.out.println("|                SALE RECEIPT             |");
+	System.out.println("|                                         |");
+	System.out.println("| 12345 MIRACLE DRIVE NEENAH, WI 54956    |");
+	System.out.println("| 692 LINDA LANE CHESHIRE. CT 06410       |");
+	System.out.println("| 80 GLENHOME ST LITTLE FALLS, NJ 07424   |");
+	System.out.println("|                                         |");
+	System.out.println("|                                         |");
+	System.out.println("|DATE                          TIME       |");
+	System.out.println("|11/16/2004                    11:16 A.M  |");
+	System.out.println("|                                         |");
+	System.out.println("|INVOICE#   166978                        |");
+	System.out.println("|AUTH#                                    |");
+	System.out.println("|                MASTERCARD               |");
+	System.out.println("|              ACCOUNT NUMBER             |");
+	System.out.println("|              ***********27              |");
+	System.out.println("|                                         |");
+	System.out.println("|   PUMP           PRODUCT           $/G  |");
+	System.out.println("|    07              UNLD            $2.5 |");
+	System.out.println("|                                         |");
+	System.out.println("|  GALLONS           FUEL           TOTAL |");
+	System.out.println("|   20.54               $           51.35 |");
+	System.out.println("|                                         |");
+	System.out.println("|                 THANK YOU!              |");
+	System.out.println("|                GAS STATION              |");
+	System.out.println("|                   SUNOCO                |");
+	System.out.println("+-----------------------------------------+");
+    }
+}
